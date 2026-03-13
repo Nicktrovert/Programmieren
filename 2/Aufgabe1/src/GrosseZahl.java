f625684c840d46c17944359b101d37f1bc04a7ce
Nick Helmers <yolorolo2007@gmail.com>
2026-03-13 15:43:50 +0100

    ggT

--- 2/Aufgabe1/src/GrosseZahl.java
+++ 2/Aufgabe1/src/GrosseZahl.java
@@ -25,39 +25,52 @@ public class GrosseZahl {
         return sb.toString();
     }
 
-    public boolean less(GrosseZahl b){ //gerrit
+    public boolean less(GrosseZahl b){ //todo (gerrit)
         // todo : implement
 
         return false;
     }
 
-    public GrosseZahl add(GrosseZahl b){ //gerrit
+    public GrosseZahl add(GrosseZahl b){ //todo (gerrit)
         // todo : implement
 
         return null;
     }
 
-    public GrosseZahl sub(GrosseZahl b){ //gerrit
+    public GrosseZahl sub(GrosseZahl b){ //todo (gerrit)
         // todo : implement
         return null;
     }
 
-    public GrosseZahl mult(GrosseZahl b){ //nick
+    public GrosseZahl mult(GrosseZahl b){ //todo (nick)
         // todo : implement
 
         return null;
     }
 
-    public GrosseZahl ggT(GrosseZahl b) throws CloneNotSupportedException { //nick
+    public GrosseZahl ggT(GrosseZahl b) throws CloneNotSupportedException {
         GrosseZahl a = (GrosseZahl) this.clone();
 
         return ggT_recursive(a, b);
     }
 
     private GrosseZahl ggT_recursive(GrosseZahl a, GrosseZahl b) {
+        GrosseZahl leftover = ggT_recursive_findLeftover(a, b);
 
+        if (leftover.less(new GrosseZahl(1))){
+            return b;
+        }
 
+        return ggT_recursive(b, leftover);
+    }
 
-        return null;
+    private GrosseZahl ggT_recursive_findLeftover(GrosseZahl a, GrosseZahl b) {
+        a = a.sub(b);
+
+        if (a.less(b)){
+            return a;
+        }
+
+        return ggT_recursive_findLeftover(a, b);
     }
 }
