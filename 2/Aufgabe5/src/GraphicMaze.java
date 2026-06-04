c06af73e33b2bf4627d1168776ac269c2b8c123c
Nicktrovert <nick.helmers07@gmail.com>
2026-06-04 18:33:50 +0200

    zwischen commit

--- 2/Aufgabe5/src/GraphicMaze.java
+++ 2/Aufgabe5/src/GraphicMaze.java
@@ -1,5 +1,55 @@
-public class GraphicMaze {
-    public static void main(String[] args){
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
 
+public class GraphicMaze extends JFrame {
+    private Graphic graphic;
+    private Maze maze;
+
+    public GraphicMaze(int width, int height) {
+        graphic = new Graphic(width, height);
+        add(graphic);
+    }
+
+    public void drawMaze(){
+        int cell_size = 30;
+
+        for (Point p : this.maze.floor){
+            graphic.setColor(Color.WHITE);
+            graphic.fillRect(p.x * cell_size, p.y * cell_size, cell_size, cell_size);
+        }
+
+        for (Point p : this.maze.walls){
+            graphic.setColor(Color.BLACK);
+            graphic.fillRect(p.x * cell_size, p.y * cell_size, cell_size, cell_size);
+        }
+
+        for (Point p : this.maze.solution){
+            graphic.setColor(Color.BLUE);
+            graphic.fillOval(p.x * cell_size, p.y * cell_size, cell_size, cell_size);
+        }
+
+        graphic.redraw();
+    }
+
+    public static void main(String[] args){
+        GraphicMaze frame = new GraphicMaze(500, 500);
+
+        frame.setTitle("GraphicMaze");
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        char[][] mazeData = {
+                {' ', '#', ' ', ' '},
+                {' ', '#', ' ', '#'},
+                {' ', ' ', ' ', '#'},
+                {'#', '#', ' ', ' '}
+        };
+
+        frame.maze = new Maze(mazeData);
+        frame.maze.canExit(0, 0);
+
+        frame.drawMaze();
     }
 }
--- 2/Aufgabe5/src/Maze.java
+++ 2/Aufgabe5/src/Maze.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 
 //todo: Rewrite class for Task
 public class Maze {
 
 	char[][] maze;
+	public List<Point> solution = new ArrayList<Point>();
+	public List<Point> walls = new ArrayList<Point>();
+	public List<Point> floor = new ArrayList<Point>();
 	
 	public Maze(char[][] maze) {
 		this.maze = maze;
@@ -15,18 +20,22 @@ public class Maze {
 		if (i < 0 || j < 0 || i >= n || j >= n)
 			return false;
 		
-		if (maze[i][j] != ' ')
+		if (maze[i][j] != ' '){
+			walls.add(new Point(i, j));
 			return false;
-		
+		}
+
 		maze[i][j] = '.';
 		
 		if ((i == n-1 && j == n-1) 
 			|| canExit(i+1, j) || canExit(i, j+1)
 			|| canExit(i-1, j) || canExit(i, j-1)) {
-			System.out.println("("+j+", "+i+")");
 			maze[i][j] = '+';
+			solution.addFirst(new Point(i, j));
 			return true;
 		}
+
+		floor.add(new Point(i, j));
 		
 		return false;
 		
